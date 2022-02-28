0f121b0bd6207150de38d43ea976fc28a304de1f
Braden Lint <bradenlint@Braden-Lints-MacBook-Pro.local>
2022-02-28 15:42:53 -0700

    initial commit

---
+++ app/src/main/java/com/example/godsstorycoloringapp/splashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.godsstorycoloringapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class splashScreen : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
